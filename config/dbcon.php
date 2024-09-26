e20c60b3ac02668eb8fa15c2f60f63fab3223706
Ayushi Sinha <sinhask139@gmail.com>
2024-09-26 16:56:21 +0530

    integrate bootstrap & frontend

--- config/dbcon.php
+++ config/dbcon.php
@@ -14,7 +14,7 @@ $con = mysqli_connect($host, $username, $password, $database);
 if(!$con){
     die("connection failed". mysqli_connect_error());
 }
-else{
-    echo "Connected Successfully";
-}
+// else{
+//     echo "Connected Successfully";
+// }
 ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php');?>  
+    <h1>Hello World</h1>
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+   
+<?php include('includes/footer.php');?>  
+  
