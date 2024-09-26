e5ad65adc5814423cd8377e896966840cd6b7c25
Ayushi Sinha <sinhask139@gmail.com>
2024-09-26 15:04:21 +0530

    first commit

---
+++ config/dbcon.php
@@ -0,0 +1,20 @@
+<?php 
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "phpecom";
+
+//creating db connection
+
+$con = mysqli_connect($host, $username, $password, $database);
+
+//check db connection
+
+if(!$con){
+    die("connection failed". mysqli_connect_error());
+}
+else{
+    echo "Connected Successfully";
+}
+?>
