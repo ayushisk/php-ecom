132f1a27a2897d81d2791f9469e559cf3e236f17
Ayushi Sinha <sinhask139@gmail.com>
2024-09-27 13:05:48 +0530

    advanced login and logout system

---
+++ register.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+
+
+if(isset($_SESSION['auth'])){
+    $_SESSION['message'] = "you are already logged In";
+    header('Location: index.php');
+    exit();
+}
+
+include('includes/header.php');
+?>  
+    
+<div class="py-5">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+        <?php 
+            if(isset($_SESSION['message'])){
+                ?>    
+                 <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                 <strong>Hey Folk!</strong> <?= $_SESSION['message']; ?>
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php
+                unset($_SESSION['message']);
+            }
+        ?>    
+
+            <div class="card">
+                <div class="card-header">
+                    <h4>Registration form</h4>
+                </div>
+            </div>
+            <div class="card-body border border-2 rounded">
+                <form class="m-4" action="functions/authcode.php" method="POST">
+                    <div class="mb-3 ">
+                        <label  class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Enter your Name">
+                    </div>
+                    <div class="mb-3 ">
+                        <label  class="form-label">Phone No.</label>
+                        <input type="number" name="phone" class="form-control" placeholder="Enter your Number">
+                    </div>
+                    <div class="mb-3 ">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your Email" aria-describedby="emailHelp">
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
+                    </div>
+                    <div class="mb-3">
+                        <label  class="form-label">Confirm Password</label>
+                        <input type="password" name="cpassword" class="form-control" placeholder="Enter your confirm password">
+                    </div>
+                    
+                    <button type="submit" name="register_btn" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+   
+<?php include('includes/footer.php');?>  
+  
