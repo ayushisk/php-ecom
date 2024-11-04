7eaaa9d519c622e4090b654c0e7616a497815f14
Ayushi Sinha <sinhask139@gmail.com>
2024-11-04 16:33:23 +0530

    Integrate Alertify JS & add category to mysql

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -18,6 +18,19 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
   <!-- CSS Files -->
   <link id="pagestyle" href="assets/css/material-dashboard.min.css" rel="stylesheet" />
+
+  <!-- alertify js -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css"/>
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/bootstrap.min.css"/>
+
+  
+  <style>
+    .form-control{
+        border: 1px solid #b3a1a1 !important;
+        padding: 8px 10px;
+    }
+  </style>
+
 </head>
 
 <body class="g-sidenav-show  bg-gray-200">
