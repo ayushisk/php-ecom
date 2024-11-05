c221890d1aaad5d2e0630bcecef135b4ac567636
Ayushi Sinha <sinhask139@gmail.com>
2024-11-05 13:10:26 +0530

    fetch and display category in php

--- functions/myfunctions.php
+++ functions/myfunctions.php
@@ -1,12 +1,51 @@
 <?php 
 
-function redirect($url, $message){
+// function getAll($table)
+// {
+//     global $con;
+//     $query = "SELECT * FROM $table";
+//     $query_run  = mysqli_query($con, $query);
+//     return $query_run;
+// }
 
-    $_SESSION['message'] = $message;
-    header('Location: '.$url);
-    exit();
 
+// function redirect($url, $message){
+
+//     $_SESSION['message'] = $message;
+//     header('Location: '.$url);
+//     exit();
+
+// }
+// chat gpt code
+
+include_once('../config/dbcon.php');
+
+if (!function_exists('getAll')) {
+    function getAll($table)
+    {
+        global $con;
+        $query = "SELECT * FROM $table";
+        $query_run  = mysqli_query($con, $query);
+        return $query_run;
+    }
 }
 
+if (!function_exists('getByID')) {
+    function getByID($table, $id)
+    {
+        global $con;
+        $query = "SELECT * FROM $table WHERE id='$id' ";
+        $query_run  = mysqli_query($con, $query);
+        return $query_run;
+    }
+}
+
+if (!function_exists('redirect')) {
+    function redirect($url, $message){
+        $_SESSION['message'] = $message;
+        header('Location: '.$url);
+        exit();
+    }
+}
 
 ?>
